1a1baba40ece42d0173888f9bec5869a7821009a
F-Myers. <fmy150504@gmail.com>
2024-08-27 10:48:20 +0700

    menambahkan frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import DataVisualization from './DataVisualization';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>React and Express Experiment</h1>
+            </header>
+            <DataVisualization />
+        </div>
+    );
+}
+
+export default App;
